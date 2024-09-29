2a1da2463d61f51ea4d0bd8d9fb0569d45d30caf
witordev <witortena@gmail.com>
2024-09-28 23:03:34 -0300

    banana

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',  // Ensures the assets use relative paths
   plugins: [react()],
 })
